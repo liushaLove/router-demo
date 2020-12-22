f8e624586efae21928ce36dabf39b36cb286fd83
liushaLove <1270842602@qq.com>
2020-12-22 23:41:35 +0800

    提交前端实现路由代码

---
+++ src/history.js
@@ -0,0 +1,58 @@
+const app = document.querySelector("#app");
+const div1 = document.createElement("div");
+div1.innerHTML = "1";
+const div2 = document.createElement("div");
+div2.innerHTML = "2";
+const div3 = document.createElement("div");
+div3.innerHTML = "3";
+const div4 = document.createElement("div");
+div4.innerHTML = "4";
+
+//建立路由表
+const routeTable = {
+    "/1": div1,
+    "/2": div2,
+    "/3": div3,
+    "/4": div4
+}
+
+function route(container){
+    
+    let number = window.location.pathname;
+    console.log("number: " + number);
+
+    if("/" === number){
+        number = "/1";
+    }
+
+    //获取界面
+    let div = routeTable[number.toString()];
+    //div404代表的就是默认路由
+    if(!div){
+        div = document.querySelector("#div404");
+    }
+    div.style.display = 'block';
+
+    // 界面展示
+    container.innerHTML = "";
+    container.appendChild(div);
+}
+
+const allA = document.querySelectorAll("a.link");
+
+for(let a of allA){
+    a.addEventListener("click",e=>{
+        e.preventDefault();
+        const href = a.getAttribute("href");
+        window.history.pushState(null,`page ${href}`,href);
+        //通知
+        onStatechange(href);
+    });
+}
+
+route(app);
+
+function onStatechange (){
+    console.log("state 变了");
+    route(app);
+}
